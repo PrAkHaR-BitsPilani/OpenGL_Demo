e7b26a31db27240556314fdd5a5a185149037924
PrAkHaR-BitsPilani <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2021-03-10 00:20:23 +0530

    coloring + performance

--- src/Bresenham.cpp
+++ src/Bresenham.cpp
@@ -1,94 +1,95 @@
 #include "Bresenham.h"
 #include <iostream>
 
-void addCirclePoints(std::vector<float>& ans , float x , float y , float z , glm::vec3 centre);
+void addCirclePoints(std::vector<float>& ans , float x , float y , float z , glm::vec3 centre , glm::vec3 color);
 
-std::vector<float> Bresenham::drawLine(glm::vec3 p1 , glm::vec3 p2)
+float abs(float f)
+{
+    if(f < 0.0f)return -f;
+    return f;
+}
+
+std::vector<float> Bresenham::drawLine(glm::vec3 p1 , glm::vec3 p2 , glm::vec3 color)
 {  
+    std::vector<float> ans;
 
     assert(p1.z == p2.z);
 
-    std::vector<float> ans;
-    bool swapFlag = false;
-    if(p1.x > p2.x){
-        glm::vec3 temp = p1;
-        p1 = p2;
-        p2 = temp;
-        swapFlag = true;
-    }
-    int delX = p2.x - p1.x;
-    int delY = p2.y - p1.y;
-    int a = delY;
-    int b = -delX;
+    float delX = p2.x - p1.x;
+    float delY = p2.y - p1.y;
+    float a = delY;
+    float b = -delX;
     if(abs(delY) <= abs(delX))
     {
+        //std::cout << "Slop less than 1\n";
         
+        int x_dir = (p1.x <= p2.x) ? 1 : -1;
         int y_dir = (p1.y <= p2.y) ? 1 : -1;
+
+        a *= x_dir;
         b *= y_dir;
 
-        int d = 2*a + b;
-        int incE = 2*a;
-        int incNE = 2*(a+b);
+        float d = 2*a + b;
+        float incE = 2*a;
+        float incNE = 2*(a+b);
 
         float x = p1.x;
         float y = p1.y;
         float z = p1.z;
 
-        ans.emplace_back(x);
-        ans.emplace_back(y);
-        ans.emplace_back(z);
+        ans.emplace_back(x);ans.emplace_back(y);ans.emplace_back(z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
 
-        while(x <= p2.x)
+        while((x_dir * x) <= (x_dir * p2.x))
         {
-            if((y_dir * d) <= 0){
+            if((x_dir * y_dir * d) <= 0){
                 d += incE;
             }else{
                 d += incNE;
                 y += y_dir * delta;
             }
-            x += delta;
+            x += x_dir * delta;
 
-            ans.emplace_back(x);
-            ans.emplace_back(y);
-            ans.emplace_back(z);
+            ans.emplace_back(x);ans.emplace_back(y);ans.emplace_back(z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
         }
     }
     else
     {
-        int y_dir = p1.y <= p2.y ? 1 : -1;
+
+        //std::cout << "Slop greater than 1\n";
+
+        int x_dir = (p1.x <= p2.x) ? 1 : -1;
+        int y_dir = (p1.y <= p2.y) ? 1 : -1;
+
+        a *= x_dir;
         b *= y_dir;
 
-        int d = a + 2*b;
-        int incN = 2*b;
-        int incNE = 2*(a+b);
+        float d = a + 2*b;
+        float incN = 2*b;
+        float incNE = 2*(a+b);
 
         float x = p1.x;
         float y = p1.y;
         float z = p1.z;
 
-        ans.emplace_back(x);
-        ans.emplace_back(y);
-        ans.emplace_back(z);
+        ans.emplace_back(x);ans.emplace_back(y);ans.emplace_back(z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
 
         while((y_dir * y) <= (y_dir * p2.y))
         {
-            if((y_dir * d) > 0){
+            if((x_dir * y_dir * d) > 0){
                 d += incN;
             }else{
                 d += incNE;
-                x += delta;
+                x += x_dir * delta;
             }
             y += y_dir * delta;
 
-            ans.emplace_back(x);
-            ans.emplace_back(y);
-            ans.emplace_back(z);
+            ans.emplace_back(x);ans.emplace_back(y);ans.emplace_back(z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
         }
     }
     return ans;
 }
 
-std::vector<float> Bresenham::drawCircle(glm::vec3 centre , int radius)
+std::vector<float> Bresenham::drawCircle(glm::vec3 centre , float radius , glm::vec3 color)
 {
     std::vector<float> ans;
 
@@ -100,7 +101,7 @@ std::vector<float> Bresenham::drawCircle(glm::vec3 centre , int radius)
     float delE = 2*delta + 0;
     float delSE = -2* radius + 2*delta + 3*0;
 
-    addCirclePoints(ans , x , y , z , centre);
+    addCirclePoints(ans , x , y , z , centre , color);
 
     while(y > x)
     {
@@ -116,50 +117,38 @@ std::vector<float> Bresenham::drawCircle(glm::vec3 centre , int radius)
         }
         x += delta;
 
-        addCirclePoints(ans , x , y , z , centre);
+        addCirclePoints(ans , x , y , z , centre , color);
     }
 
     return ans;
 }
 
-std::vector<float> Bresenham::drawAxis(int max)
+std::vector<float> Bresenham::drawAxis(int max , glm::vec3 color)
 {
     std::vector<float> ans;
     float x = 0.0f;
     while(x <= max)
     {
-        ans.emplace_back(x);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(x);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(x);
-        ans.emplace_back(-x);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(-x);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(0.0f);
-        ans.emplace_back(-x);
+        ans.emplace_back(x);ans.emplace_back(0.0f);ans.emplace_back(0.0f);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+        ans.emplace_back(0.0f);ans.emplace_back(x);ans.emplace_back(0.0f);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+        ans.emplace_back(0.0f);ans.emplace_back(0.0f);ans.emplace_back(x);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+        ans.emplace_back(-x);ans.emplace_back(0.0f);ans.emplace_back(0.0f);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+        ans.emplace_back(0.0f);ans.emplace_back(-x);ans.emplace_back(0.0f);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+        ans.emplace_back(0.0f);ans.emplace_back(0.0f);ans.emplace_back(-x);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
         x += delta;
     }
     return ans;
 }
 
-void addCirclePoints(std::vector<float>& ans , float x , float y , float z , glm::vec3 centre){
-    ans.emplace_back(centre.x + x);ans.emplace_back(centre.y + y);ans.emplace_back(centre.z + z);
-    ans.emplace_back(centre.x + x);ans.emplace_back(centre.y + -y);ans.emplace_back(centre.z + z);
-    ans. emplace_back(centre.x + -x);ans.emplace_back(centre.y + y);ans.emplace_back(centre.z + z);
-    ans.emplace_back(centre.x + -x);ans.emplace_back(centre.y + -y);ans.emplace_back(centre.z + z);
+void addCirclePoints(std::vector<float>& ans , float x , float y , float z , glm::vec3 centre , glm::vec3 color){
+    ans.emplace_back(centre.x + x);ans.emplace_back(centre.y + y);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+    ans.emplace_back(centre.x + x);ans.emplace_back(centre.y + -y);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+    ans. emplace_back(centre.x + -x);ans.emplace_back(centre.y + y);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+    ans.emplace_back(centre.x + -x);ans.emplace_back(centre.y + -y);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
 
-    ans.emplace_back(centre.x + y);ans.emplace_back(centre.y + x);ans.emplace_back(centre.z + z);
-    ans.emplace_back(centre.x + y);ans.emplace_back(centre.y + -x);ans.emplace_back(centre.z + z);
-    ans.emplace_back(centre.x + -y);ans.emplace_back(centre.y + x);ans.emplace_back(centre.z + z);
-    ans.emplace_back(centre.x + -y);ans.emplace_back(centre.y + -x);ans.emplace_back(centre.z + z);
+    ans.emplace_back(centre.x + y);ans.emplace_back(centre.y + x);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+    ans.emplace_back(centre.x + y);ans.emplace_back(centre.y + -x);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+    ans.emplace_back(centre.x + -y);ans.emplace_back(centre.y + x);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
+    ans.emplace_back(centre.x + -y);ans.emplace_back(centre.y + -x);ans.emplace_back(centre.z + z);ans.emplace_back(color.r);ans.emplace_back(color.g);ans.emplace_back(color.b);
 
 }
--- src/Bresenham.h
+++ src/Bresenham.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <vector>
+#include <utility>
 #include "external/glm/glm.hpp"
 
-#define delta 0.001f
-#define invDelta 1000
-#define invDeltaSQ 1000000
+#define delta 0.01f
 
 namespace Bresenham
 {
-    std::vector<float> drawAxis(int max);
-    std::vector<float> drawLine(glm::vec3 p1 , glm::vec3 p2);
-    std::vector<float> drawCircle(glm::vec3 centre , int radius);
+    std::vector<float> drawAxis(int max , glm::vec3 color);
+    std::vector<float> drawLine(glm::vec3 p1 , glm::vec3 p2 , glm::vec3 color);
+    std::vector<float> drawCircle(glm::vec3 centre , float radius , glm::vec3 color);
 }
